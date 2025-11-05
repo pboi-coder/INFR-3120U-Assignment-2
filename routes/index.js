8a9ab815602fb77e20410e44393814376474583c
Param Sharma <theparamsharma@gmail.com>
2025-11-05 17:59:54 -0500

    Completed initial portfolio pages

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* Home page */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+/* About page */
+router.get('/about', function(req, res) {
+  res.render('about');
+});
+
+/* Projects page */
+router.get('/projects', function(req, res) {
+  res.render('projects');
+});
+
+/* Contact page */
+router.get('/contact', function(req, res) {
+  res.render('contact');
+});
+
+module.exports = router;
